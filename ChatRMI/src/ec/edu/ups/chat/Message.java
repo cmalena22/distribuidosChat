f99610e1309c2965e8d105c01dd5882aff2d2c1b
Cinthia Iza <cizac1@est.ups.edu.ec>
2021-04-19 21:00:29 -0500

    Ventanas

--- ChatRMI/src/ec/edu/ups/chat/Message.java
+++ ChatRMI/src/ec/edu/ups/chat/Message.java
@@ -1,5 +1,53 @@
 package ec.edu.ups.chat;
 
-public class Message {
+import java.io.Serializable;
+import java.util.Date;
 
+public class Message implements Serializable {
+	private String username;
+    private String msg;
+    private String type;
+    private Date date;
+    private String smiley;
+    
+    public String getSmiley() {
+        return smiley;
+    }
+
+    public void setSmiley(String smiley) {
+        this.smiley = smiley;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
 }
--- ChatRMI/src/ec/edu/ups/chat/chatInterface.java
+++ ChatRMI/src/ec/edu/ups/chat/chatInterface.java
@@ -1,5 +1,13 @@
 package ec.edu.ups.chat;
 
-public interface chatInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
+public interface chatInterface  extends Remote{
+	// declaring methods to call remotely
+    public boolean login(String username) throws RemoteException;
+    public void sendMessage(Message message) throws RemoteException;
+    public List<Message> getAllMessages() throws RemoteException;
+    public List<String> getAllUsers() throws RemoteException;
 }
